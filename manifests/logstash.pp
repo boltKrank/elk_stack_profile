245a65fa29c8aa79f6cd492c5cb550aa93e122d2
Simon Anderson <simon.anderson111@gmail.com>
2019-11-15 09:54:25 +1100

    added classes

---
+++ manifests/logstash.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include elk_stack_profile::logstash
+class elk_stack_profile::logstash {
+}
